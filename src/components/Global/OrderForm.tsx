c8f20e5f80d490129515a0fd8c4493b310443539
huangzhaoxin <1614629345@qq.com>
2023-06-07 18:16:23 +0800

    fixed

--- src/components/Global/OrderForm.tsx
+++ src/components/Global/OrderForm.tsx
@@ -1,78 +1,54 @@
-import { Space, Table, Tag } from 'antd';
-import type { ColumnsType } from 'antd/es/table';
+import {Space, Table, Tag} from 'antd';
+import type {ColumnsType} from 'antd/es/table';
 
+interface DataType {
+    id: string,
+    customerId: string,
+    orderDate: string,
+    expectedTime: string,
+    status: string,
+    productList: Array<{ id: string, productName: string, quantity: number }>
+}
 
-export const OrderForm = (props:{
+const data: DataType[] = [
+    {
+        id: '1234',
+        customerId: 'Singapore',
+        orderDate: '2020-3-4',
+        expectedTime: '2020-3-6',
+        status: 'wait for raw',
+        productList: [{id: '2222', productName: 'MIX CHOC DRINK KR 560G 16/CS', quantity: 4000}]
+    }, {
+        id: '1234',
+        customerId: 'Singapore',
+        orderDate: '2020-3-4',
+        expectedTime: '2020-3-6',
+        status: 'wait for raw',
+        productList: [{id: '2222', productName: 'MIX CHOC DRINK KR 560G 16/CS', quantity: 4000}]
+    }
+];
+
+export const OrderForm = (props: {
     role: string, //根据角色请求不同数据
     type: string, //根据type自定义表头以及最后一列的操作，定义能否多选
     changePage?: Function, //分页函数
 }) => {
-    interface DataType {
-        key: string;
-        name: string;
-        age: number;
-        address: string;
-        tags: string[];
-    }
 
-    const data: DataType[] = [
-        {
-            key: '1',
-            name: 'John Brown',
-            age: 32,
-            address: 'New York No. 1 Lake Park',
-            tags: ['nice', 'developer'],
-        },
-        {
-            key: '2',
-            name: 'Jim Green',
-            age: 42,
-            address: 'London No. 1 Lake Park',
-            tags: ['loser'],
-        },
-        {
-            key: '3',
-            name: 'Joe Black',
-            age: 32,
-            address: 'Sydney No. 1 Lake Park',
-            tags: ['cool', 'teacher'],
-        },
-    ];
     const columns: ColumnsType<DataType> = [
         {
-            title: 'Name',
-            dataIndex: 'name',
-            key: 'name',
+            title: 'Department',
+            dataIndex: 'customerId',
+            key: 'customerId',
         },
         {
-            title: 'Age',
-            dataIndex: 'age',
-            key: 'age',
+            title: 'Expected Date',
+            dataIndex: 'expectedTime',
+            key: 'expectedTime',
         },
         {
-            title: 'Address',
-            dataIndex: 'address',
-            key: 'address',
-        },
-        {
-            title: 'Tags',
-            key: 'tags',
-            dataIndex: 'tags',
-            render: (_, { tags }) => (
-                <>
-                    {tags.map((tag) => {
-                        let color = tag.length > 5 ? 'geekblue' : 'green';
-                        if (tag === 'loser') {
-                            color = 'volcano';
-                        }
-                        return (
-                            <Tag color={color} key={tag}>
-                                {tag.toUpperCase()}
-                            </Tag>
-                        );
-                    })}
-                </>
-            ),
+            title: 'Status',
+            dataIndex: 'status',
+            key: 'status',
         },
         {
             title: 'Action',
@@ -84,7 +60,7 @@ export const OrderForm = (props:{
             ),
         },
     ];
-    return <Table columns={columns} dataSource={data} />
+    return <Table columns={columns} dataSource={data}/>
 }
 
 
--- src/pages/order/[role].tsx
+++ src/pages/order/[role].tsx
@@ -2,12 +2,13 @@ import {NextPageWithLayout} from "../_app";
 import {useRouter} from "next/router";
 import {getLayout} from "@/components/Layout";
 import {OrderForm} from "@/components/Global/OrderForm";
+import styles from "@/styles/order.module.css"
 
 const Order: NextPageWithLayout = () => {
     const router = useRouter()
     return (
-        <div>
-
+        <div className={styles.container}>
+            <OrderForm role={'commercial'} type={'order'}></OrderForm>
         </div>
     )
 }
